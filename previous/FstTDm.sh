98d33137de5c057442794d49009ce2c3dffeab9d
micko-gricko <52712526+micko-gricko@users.noreply.github.com>
2019-11-11 18:18:17 +0100

    Update FstTDm.sh

--- previous/FstTDm.sh
+++ previous/FstTDm.sh
@@ -22,4 +22,4 @@ module load UHTS/Analysis/vcftools/0.1.15
 #outputs like freq depth and  allele counts FST and Tajimas D have to be run in separate scripts - VCFtools suport only 1 output - repeat the same filters
 # I used Bed tools to check the depth for my samples and then set minDP and maxDP. I used sam minQ as I did for mapping
 
-vcftools --vcf Tcm1.fb.vcf --remove-indv tcm_m32.bam --mac 3 --MinQ 30 --minDP 5 --maxDP 50 --freq --counts --depth --weir-fst-pop tcm1.txt --weir-fst-pop tcm2.txt --TajimaD 1000 
+vcftools --vcf Tcm1.fb.vcf --remove-indv tcm_m32.bam --mac 3 --minQ 30 --minDP 5 --maxDP 50 --freq --counts --depth --weir-fst-pop tcm1.txt --weir-fst-pop tcm2.txt --TajimaD 1000 
